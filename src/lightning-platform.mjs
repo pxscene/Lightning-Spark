8e31a7c9f323350b18162b53aecda6be7d250877
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-07-17 09:54:55 -0400

    cleanup

--- src/lightning-platform.mjs
+++ src/lightning-platform.mjs
@@ -1,8 +1,5 @@
-//import lng from "wpe-lightning/src/lightning.mjs";
 import SparkPlatform from "./platforms/spark/SparkPlatform.mjs"
 
 const lightning = SparkPlatform;
-
-//lightning.Stage.platform = SparkPlatform;
 
 export default lightning;
