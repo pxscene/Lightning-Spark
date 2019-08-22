571dd5c4e994795cd65ed6e3b9a43da6fc4ff7c6
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-08-22 09:42:52 -0400

    clean up plugins

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,6 @@ const resolve = require('rollup-plugin-node-resolve');
 
 export default {
     input: './src/lightning-platform-spark.mjs',
-    plugins: [resolve({
-        only: [ 'wpe-lightning' ]
-    })],
     output: {
         file: './dist/lightning-spark.js',
         format: 'cjs',
