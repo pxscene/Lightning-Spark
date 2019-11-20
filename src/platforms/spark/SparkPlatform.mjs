c57a3e467d4ba344433fc9473d06354952d94f29
Michael Fiess <10201495+mfiess@users.noreply.github.com>
2019-11-20 08:36:26 -0500

    Revert "SparkMediaplayer support"

--- src/platforms/spark/SparkPlatform.mjs
+++ src/platforms/spark/SparkPlatform.mjs
@@ -59,7 +59,6 @@ export default class SparkPlatform {
     createRoundRect(cb, stage, w, h, radius, strokeWidth, strokeColor, fill, fillColor) {
         if (fill === undefined) fill = true;
         if (strokeWidth === undefined) strokeWidth = 0;
-        if (fillColor === undefined) fillColor = 0;
 
         fillColor = fill ? fillColor : 0;
         fillColor = fillColor.toString(16);
